850ceca2a81d21e0ff0dc1901793115444f27684
torabTech <53045466+torabTech@users.noreply.github.com>
2022-02-05 11:08:46 -0600

    initial commit

--- src/main/java/com/test/springbootgithubactions/controller/StudentController.java
+++ src/main/java/com/test/springbootgithubactions/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/students")
+@RequestMapping("/api/students")
 @RestController
 public class StudentController {
     private final StudentService studentService;
